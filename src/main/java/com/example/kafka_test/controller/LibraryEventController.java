1d93e33dc897b804161165fdd33e6e05915cd0f0
10839301 <ayyaz.shaikh@ltimindtree.com>
2025-12-24 20:00:15 +0530

    changes for kafka

--- src/main/java/com/example/kafka_test/controller/LibraryEventController.java
+++ src/main/java/com/example/kafka_test/controller/LibraryEventController.java
@@ -1,6 +1,8 @@
 package com.example.kafka_test.controller;
 
 import com.example.kafka_test.domain.LibraryEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.logging.Log;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class LibraryEventController {
 
     @PostMapping("/v1/libraryEvent")
@@ -16,6 +19,7 @@ public class LibraryEventController {
             @RequestBody LibraryEvent libraryEvent
     )
     {
+        log.info("libraryEvent {} " , libraryEvent  );
     // invoke kafka producer
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
